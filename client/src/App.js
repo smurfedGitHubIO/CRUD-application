cef3d20f2b1f2f85acfa087dffa687e7d9714723
Justin Clyde V. Frongoso <clydefrongoso@gmail.com>
2021-11-28 18:28:56 +0800

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,16 @@
 import './App.css';
 import { useState } from "react";
-import Axios from "axios";
+import { useEffect } from "react";
+import axios from "axios";
 
 function App() {
-  const [name, setName] = useState("");
-  const [age, setAge] = useState(0);
-
-  const displayInfo = () => {
-    console.log(name + age);
-  };
-
-  const addData = () => {
-    Axios.post('localhost:3307/create', {name: name, age: age}).then(() => {
-      console.log("Success");
+  useEffect(() => {
+    axios.get("localhost:3002/posts").then((response) => {
+      console.log(response);
     });
-  };
-
-  return (
-    <div className="App">
-      <div className="inputs">
-        <label>Name:</label>
-        <input
-          type="text"
-          onChange={(event) => {
-            setName(event.target.value)
-          }}
-        />
-        <label>Age:</label>
-        <input
-          type="number"
-          onChange={(event) => {
-            setAge(event.target.value)
-          }}
-        />
-        <button onClick={addData}>Submit</button>
-      </div>
-    </div>
-  );
+  }, []);
+  return 
+    <div className="App"></div>;
 }
 
 export default App;
