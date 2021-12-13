d7a4c782bd650f4c385d489c2c6059a4a07783eb
Justin Clyde V. Frongoso <clydefrongoso@gmail.com>
2021-12-13 21:09:29 +0800

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -1,19 +1,57 @@
 const express = require('express');
 const app = express();
-const mysql = require('mysql2');
+const mysql = require('mysql');
+const mysql2 = require('mysql2');
 const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
 
-const db = require('./models');
+// const db = require('./models');
 
 // Routers
-const postRouter = require('./routes/posts');
-app.use("/posts", postRouter);
+// const postRouter = require('./routes/posts');
+// app.use("/posts", postRouter);
+//const registeredUsersRouter = require('./routes/registered-users');
+//app.use("/auth", registeredUsersRouter);
 
-db.sequelize.sync().then(() => {
-	app.listen(3002, () => {
-		console.log("Server running on port 3002.");
-	});
+// db.sequelize.sync().then(() => {
+// });
+
+const db = mysql.createConnection({
+	user: "root",
+	host: "localhost",
+	password: "",
+	database: "LEMONS",
+});
+
+app.post('/create', (req, res) => {
+	const name = req.body.name;
+	const age = req.body.age;
+	db.query("INSERT INTO random (name, age) VALUES (?, ?)", [name, age],
+		(err, result) => {
+			if(err){
+				console.log(err);
+			}
+			else{
+				console.log("Values inserted.");
+			}
+		}
+	);
+});
+
+app.get('/get_data', (req, res) => {
+	var lst;
+	db.query("SELECT * FROM random WHERE name='wews'", function (err, result){
+		if(err){
+			console.log(err);
+		}
+		else{
+			res.send(result);
+		}
+	})
+});
+
+app.listen(3002, () => {
+	console.log("Server running on port 3002.");
 });
