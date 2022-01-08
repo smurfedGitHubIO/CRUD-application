1509fcde9120460172c42ab5a6e7714c3678156a
Justin Clyde V. Frongoso <clydefrongoso@gmail.com>
2022-01-08 20:56:19 +0800

    Add files via upload

--- server/anotherindex.js
+++ server/anotherindex.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 
 app.post('/get_third_game_data', (req, res) => {
-	const analysis = {total_play_time : [""]};
+	const analysis = {total_play_time : ["The decreasing time that this player spends in playing the mini-game per game use may mean that he/she is progressively learning about the concept of the six basic emotions. This may also mean that he/she is a fast learner, or that he/she just does not want to do the same thing repeatedly (easy to become bored at something done repeatedly), which may be because he/she does not enjoy it.", "The increasing time that this player spends in playing the mini-game per game use may mean that he/she is struggling to learn about the concept of the six basic emotions such that he/she needs to be familiarized about it repeatedly. This may also mean that he/she is a slow learner, or that he/she just wants to do the same thing repeatedly because he/she enjoys it.", "Since the amount of time that this player spends in playing the mini-game per game use shows neither an increasing nor decreasing pattern, it may mean that the player is struggling to remember or learn about the concepts of the six basic emotions. It may also mean that the player does not seriously play it or the player does something else at some times, which can be both caused by the player being easily distracted."]};
 	const name = req.body.name;
 	var res_values; //average per level, total time for each level, total time, average play time, characters chosen, wrong clicks per level, attempts per level
 	const query = "SELECT * FROM third-game-stats WHERE name = " + name;
@@ -48,8 +48,25 @@ app.post('/get_third_game_data', (req, res) => {
 			}
 			//check game time if increasing, decreasing or neither
 			var isIncOrDec = 0;
-			for(var i=0; i<play_time_list.length; i++){
-
+			for(var i=1; i<play_time_list.length; i++){
+				if(i == 1){
+					if(play_time_list[i] > play_time_list[i-1]){
+						isIncOrDec = 1;
+					}
+					else{
+						isIncOrDec = 0;
+					}
+				}
+				else{
+					if(play_time_list[i] >= play_time_list[i-1] && isIncOrDec == 0){
+						isIncOrDec = 2;
+						break;
+					}
+					else if(play_time_list[i] <= play_time_list[i-1] && isIncOrDec == 1){
+						isIncOrDec = 2;
+						break;
+					}
+				}
 			}
 			//end of check
 			var average_time_per_level = [0,0,0,0,0,0,0];
