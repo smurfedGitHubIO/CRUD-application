e9d2b5f102e0c709e15daeaa5a2b591f7d3fb3fe
Justin Clyde V. Frongoso <clydefrongoso@gmail.com>
2022-01-08 20:33:13 +0800

    Add files via upload

---
+++ server/anotherindex.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const mysql2 = require('mysql2');
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+
